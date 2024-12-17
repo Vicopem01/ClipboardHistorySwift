08dd819be04718c2a7ae08be642801c6facc9cd4
victor <vicopem@gmail.com>
2024-12-17 01:30:51 -0500

    Initial Commit

---
+++ ClipboardHistoryv2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ClipboardHistoryv2
+//
+//  Created by Ace ♠️ on 17/12/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ ClipboardHistoryv2Tests/ClipboardHistoryv2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardHistoryv2Tests.swift
+//  ClipboardHistoryv2Tests
+//
+//  Created by Ace ♠️ on 17/12/2024.
+//
+
+import Testing
+@testable import ClipboardHistoryv2
+
+struct ClipboardHistoryv2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
